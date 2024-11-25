e6f25fe577118e8fb4c27db0e4c782e9a7e26923
Robin van Diesen <robin@meritcircle.io>
2024-11-25 15:46:10 +0100

    feat: added pages

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WhishlistPage = async () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6">Je hebt nog geen favorieten.
+      Voeg een artikel toe aan je favorieten om het te zien op deze pagina.</h1>
+    </div>
+  );
+};
+
+export default WhishlistPage;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+
+export const Header = () => {
+  return (
+    <header className='sticky top-0 z-[100] bg-black'>
+      <nav className='flex justify-between items-center h-16 mx-auto max-w-screen-xl px-6  py-1'>
+        <Link className='flex items-center h-full text-white order-10 sm:order-none' href="/">
+          <img
+            alt='ViaBovag Logo'
+            loading='lazy'
+            width='38'
+            height='57'
+            decoding='async'
+            data-nimg='1'
+            className='object-contain w-auto h-full flex-shrink-0 hidden sm:block'
+            src='https://www.viabovag.nl/next-images/logo/logo-bovag.svg'
+          ></img>
+          <img
+            alt='viaBOVAG.nl'
+            loading='lazy'
+            width='120'
+            height='17'
+            decoding='async'
+            data-nimg='1'
+            className='w-24 sm:w-32 sm:ml-1'
+            src='https://www.viabovag.nl/next-images/logo/logo-bovag-text.svg'
+          ></img>
+        </Link>
+        <Link
+          className='relative order-[30] justify-self-end flex-shrink-0'
+          href='/whishlist'
+        >
+          <span className='flex items-center justify-center h-6 w-6 relative'>
+            <svg
+              className='absolute top-0 right-0 w-full h-full fill-white'
+              viewBox='0 0 24 24'
+            >
+              <use xlinkHref='#icon--heart'></use>
+              <svg id='icon--heart' viewBox='0 0 24 24'>
+                <path d='M21.2 3.4a7.5 7.5 0 00-2-1.5 5.7 5.7 0 00-6.6 1.5l-.6.7-.6-.7a5.8 5.8 0 00-4.3-2c-1.6 0-3.2.7-4.3 2A7 7 0 001 8.3a7 7 0 001.8 4.9l8.8 10 .4.2.4-.2 8.8-9.9A7.2 7.2 0 0023 8.4c0-1.9-.6-3.7-1.8-5z'></path>
+              </svg>
+            </svg>
+            <span
+              className='text-black font-semibold relative -translate-y-[1px] '
+              data-testid='header-favorite-count'
+            >1</span>
+          </span>
+        </Link>
+      </nav>
+    </header>
+  );
+};
